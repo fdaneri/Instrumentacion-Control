a972659b9c08e7bcee8751d71e1cff68320ef0bf
jpquintana <42654427+jpquintana@users.noreply.github.com>
2018-08-31 16:08:32 -0300

    Es el progrma que genera un seno que hiso nacho y mando por mail

---
+++ PrimeraClase/seno.py
@@ -0,0 +1,37 @@
+import sounddevice as sd
+import numpy as np
+import pylab as plt
+import time
+from scipy.signal import find_peaks
+from scipy import signal
+
+def seno(frecuencia, duracion, amplitud=1, fs=192000):
+    """
+    Esta función tiene como output una funcion seno de una cierta duración y frecuencia.
+    """
+    tiempo=np.arange(fs*duracion)/fs
+    seno=amplitud*np.sin(2*np.pi*frecuencia*tiempo).astype(np.float32)
+    
+    print(plt.plot(tiempo,seno))
+    
+    return seno
+    
+def barrido_frec(frec=0,fmax=30000):
+    
+    fmin=frec
+    frecuencia=[]
+    while frec < fmax:
+        frecuencia.append(np.exp(frec))
+        frec+=2
+    
+    #print(plt.plot(frecuencia))
+
+
+def barrido_frec1 (frec=0,fmax=30000):
+    
+    while frec < fmax:
+        seno(frec)
+        frec+=2
+    
+    #print(plt.plot(frecuencia))
+        
