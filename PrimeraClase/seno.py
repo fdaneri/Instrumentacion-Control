8e6b5d8db0b4624baa4c409bade586069246dfe3
IgnacioSallaberry <42651786+IgnacioSallaberry@users.noreply.github.com>
2018-09-05 18:26:35 -0300

    Acomodé
    Acomodé funciones
    1)seno
    2)maximos del seno
    3)grabacion
    4)maximos de grabacion
    5)array exponencial de frecuencias

--- PrimeraClase/seno.py
+++ PrimeraClase/seno.py
@@ -21,72 +21,6 @@ def seno(frecuencia, duracion, amplitud=1, fs=192000):
     
     return seno
 
-def grabacion(duracion, fs=192000):
-    """
-    Graba la entrada de microfono por el tiempo especificado
-    """
-    sd.default.samplerate = fs #frecuencia de muestreo
-    sd.default.channels = 1#1 porque la entrada es una sola
-    
-    grab = sd.rec(frames = fs*duracion, blocking = True)
-    tiempo=np.arange(fs*duracion)/fs
-    
-    
-    #sd.default.channels = 1 #por las dos salidas de audio
-    #grab = sd.playrec(seno, blocking=True)
-    
-    #grafico señal y maximos       
-    plt.plot(tiempo,grab,'*')
-    plt.title('Grabacion')
-    plt.xlabel('Tiempo(s)')
-    plt.ylabel('Amplitud')
-    plt.show()    
-    
-    return grab
-    
-
-
-
-
-def max_grabacion(duracion, amplitud=1, fs=192000):
-    '''
-    Calcula los maximos de la grabacion del microfono
-    '''
-    
-    grab=grabacion(duracion, fs=192000)
-    
-    #array del tiempo
-    tiempo=np.arange(fs*duracion)/fs
-    
-    maximos=[]   #array de los maximos de la señal
-    ubic_max=[]  #array de la ubicación de los maximos
-    
-    i=1    # i recorre todo el array del seno
-
-    #busco los primeros 10 maximos----> j<10
-    while i<len(grab)-1:
-        
-        if grab[i-1] < grab[i] and grab[i] > grab[i+1]:
-            maximos.append(grab[i])        #guardo los maximos de la señal 
-            ubic_max.append(tiempo[i])    #guardo la ubicación de los maximos de la señal
-            
-        
-        i+=1
-    
-    #axes=plt.gca()
-    #axes.set_xlim([0.249,0.52])
-    #axes.set_ylim([0.999,1.02])
-    
-    
-    #grafico señal y maximos       
-    plt.plot(tiempo,grab,'*')
-    plt.title('Grabacion y maximos')
-    plt.xlabel('Tiempo(s)')
-    plt.ylabel('Amplitud')
-    plt.plot(ubic_max[:],maximos[:], 'rs')
-    plt.show()    
-    
-    
 
 
 def max_sin(frecuencia, duracion, amplitud=1, fs=192000):
@@ -129,8 +63,73 @@ def max_sin(frecuencia, duracion, amplitud=1, fs=192000):
     
     return j,maximos  # pido ver cuantos maximos tengo y sus valores
 
+#%%
 
+def grabacion(duracion, fs=192000):
+    """
+    Graba la entrada de microfono por el tiempo especificado
+    """
+    sd.default.samplerate = fs #frecuencia de muestreo
+    sd.default.channels = 1#1 porque la entrada es una sola
     
+    grab = sd.rec(frames = fs*duracion, blocking = True)
+    tiempo=np.arange(fs*duracion)/fs
+    
+    
+    #sd.default.channels = 1 #por las dos salidas de audio
+    #grab = sd.playrec(seno, blocking=True)
+    
+    #grafico señal y maximos       
+    plt.plot(tiempo,grab,'*')
+    plt.title('Grabacion')
+    plt.xlabel('Tiempo(s)')
+    plt.ylabel('Amplitud')
+    plt.show()    
+    
+    return grab
+    
+
+def max_grabacion(duracion, amplitud=1, fs=192000):
+    '''
+    Calcula los maximos de la grabacion del microfono
+    '''
+    
+    grab=grabacion(duracion, fs=192000)
+    
+    #array del tiempo
+    tiempo=np.arange(fs*duracion)/fs
+    
+    maximos=[]   #array de los maximos de la señal
+    ubic_max=[]  #array de la ubicación de los maximos
+    
+    i=1    # i recorre todo el array del seno
+
+    #busco los primeros 10 maximos----> j<10
+    while i<len(grab)-1:
+        
+        if grab[i-1] < grab[i] and grab[i] > grab[i+1]:
+            maximos.append(grab[i])        #guardo los maximos de la señal 
+            ubic_max.append(tiempo[i])    #guardo la ubicación de los maximos de la señal
+            
+        
+        i+=1
+    
+    #axes=plt.gca()
+    #axes.set_xlim([0.249,0.52])
+    #axes.set_ylim([0.999,1.02])
+    
+    
+    #grafico señal y maximos       
+    plt.plot(tiempo,grab,'*')
+    plt.title('Grabacion y maximos')
+    plt.xlabel('Tiempo(s)')
+    plt.ylabel('Amplitud')
+    plt.plot(ubic_max[:],maximos[:], 'rs')
+    plt.show()    
+    
+    
+
+#%%  
 def frecuencias(frec=0,fmax=30000):
     
     fmin=frec
@@ -144,23 +143,4 @@ def frecuencias(frec=0,fmax=30000):
     print(plt.plot(frecuencia))
 
 
-def barrido_frec1 (frec=0,fmax=30000):
-    
-    while frec < fmax:
-        seno(frec)
-        frec+=2
-    
-    #print(plt.plot(frecuencia))
-        
-#%%
-def record(duracion, fs=44100):
-    """
-    Graba la entrada de microfono por el tiempo especificado
-    """
-    sd.default.samplerate = fs #frecuencia de muestreo
-    sd.default.channels = 2 #1 porque la entrada es una sola
-    
-    grabacion = sd.rec(frames = fs*duracion, blocking = True)
-    return grabacion    
-    
     
