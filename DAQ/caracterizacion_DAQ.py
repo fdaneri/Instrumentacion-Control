c478b6824b9b024ac133fc65b0f16f4f1c6d9528
Florencia Daneri <daneriflorencia@gmail.com>
2018-10-15 19:19:35 -0300

    agrego última versión del código para DAQ

--- DAQ/caracterizacion_DAQ.py
+++ DAQ/caracterizacion_DAQ.py
@@ -4,37 +4,88 @@ import pylab as plt
 import nidaqmx
 #probamos un diagnostico para ver si reconoce la placa DAQ y que nombre le está dando
 #------------         Diagnostico      ------------
-#correr esto primero  y ver que nombre le pone al device!! ej:DevX 
-from nidaqmx import system
+#correr esto primero  y ver que nombre le pone al device!!
+from nidaqmx import system, constants
 s = system.System()
 print(list(s.devices)) 
 
-#ai0 y ai1 son los dos inputs analógicos del DAQ
+
 with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("Dev7/ai1")
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
     print(task.read())
 
-#lee voltaje del canal analógico que se especifica 
-#ver cómo podemos hacer que el number_of_samples_per_channel sea ALL
+
+#medimos de a dos canales
 with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("Dev7/ai0")
-    data0 = task.read(number_of_samples_per_channel=1000)
-    #print(data)
-    plt.plot(data0, label = 'Canal 0')
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0", 
+                      terminal_config=constants.TerminalConfiguration.RSE)  #pusimos esta linea para setear el modo de medición
+    task.timing.cfg_samp_clk_timing(48000) #agregamos frecuencia de sample rate, el maximo es 48000
+    
+    data0 = task.read(number_of_samples_per_channel=200)
+    plt.plot(data0,'bs-', label = 'Canal 0')
     plt.legend(loc='upper right')
-    #plt.show()
+    plt.show()
 
 
 with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("Dev7/ai1")
+    
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai1", 
+                      terminal_config=constants.TerminalConfiguration.RSE)  #pusimos esta linea para setear el modo de medición
+    task.timing.cfg_samp_clk_timing(48000) #agregamos frecuencia de sample rate
+    
+    data1 = task.read(number_of_samples_per_channel=200)
+    plt.plot(data1,'sr-', label = 'Canal 1')
+    plt.legend(loc='upper right')
+    plt.show()
+    
+    
+
+
+
+'''
+
+#canal 0
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0", 
+                      terminal_config=constants.TerminalConfiguration.RSE)  #pusimos esta linea para setear el modo de medición
+    task.timing.cfg_samp_clk_timing(48000) #agregamos frecuencia de sample rate
+    data0 = task.read(number_of_samples_per_channel=1000)
+    plt.plot(data0,'sr-', label = 'Canal 0')
+    plt.legend(loc='upper right')
+    plt.show()
+
+
+#canal 1
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai1")
     data1 = task.read(number_of_samples_per_channel=1000)
-    #print(data)
     plt.plot(data1, label = 'Canal 1')
     plt.legend(loc='upper right')
-    #plt.show()
+    plt.show()
+   
 
 
-#al final no usamos esto por ahora, estamos emitiendo la señal con un generador de funciones
+#frecuencia
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_freq_voltage_chan("Dev1/ai0",units=constants.FrequencyUnits.HZ)
+
+    freq= task.read(number_of_samples_per_channel=1000)
+    plt.plot(freq,'sr-', label = 'freq')
+    plt.legend(loc='upper right')
+    plt.show()
+
+ 
+i=0
+data=[]
+while i<len(data0):
+    data.append(data1[i]-data0[i])
+    i+=1
+
+plt.plot(data, label = 'data1-data0')
+plt.legend(loc='upper right')
+#plt.show()
+
+
 #--------------   Código para generar la señal   --------------
 import sounddevice as sd 
 import numpy as np 
@@ -95,7 +146,7 @@ def playrec_tone(frecuencia, duracion, amplitud=0.1, fs=192000):
        
       return tiempo, data, grabacion 
 
-
+'''
 
     
     
